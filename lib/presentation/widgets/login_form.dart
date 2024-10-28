ab25579055575da0661906f79e90055bb0e2de4c
Pgenrique7 <pgenrique7@gmail.com>
2024-10-28 20:09:47 +0100

    ajuste de ancho

--- lib/presentation/widgets/login_form.dart
+++ lib/presentation/widgets/login_form.dart
@@ -37,10 +37,12 @@ class _LogInFormState extends State<LogInForm> {
   void _validateAndNavigate(BuildContext context) {
     final username = _usernameController.text;
     final password = _passwordController.text;
-    final usuarioProvider = Provider.of<UsuarioProvider>(context, listen: false);
+    final usuarioProvider =
+        Provider.of<UsuarioProvider>(context, listen: false);
 
     if (username.isEmpty || password.isEmpty) {
-      _showErrorDialog(context, 'Por favor, ingrese un nombre de usuario y contraseña.');
+      _showErrorDialog(
+          context, 'Por favor, ingrese un nombre de usuario y contraseña.');
     } else if (usuarioProvider.verificarCredenciales(username, password)) {
       context.goNamed(WelcomeScreen.name);
     } else {
@@ -51,42 +53,30 @@ class _LogInFormState extends State<LogInForm> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        padding: const EdgeInsets.all(16.0),
-        margin: const EdgeInsets.symmetric(horizontal: 20.0), 
-        decoration: BoxDecoration(
-          color: Colors.white, 
-          borderRadius: BorderRadius.circular(15.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1), 
-              blurRadius: 10,
-              offset: const Offset(0, 5),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(
+            child: Center(
+              child: Text(
+                'Log in',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-          ],
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(
-             child: Center(
-              child: Text('Log in', style: TextStyle(fontSize: 20),), 
-             ),
-            ),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: 0.8 * MediaQuery.of(context).size.width, 
-              child: LogInUsername(controller: _usernameController),
-            ),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: 0.8 * MediaQuery.of(context).size.width,
-              child: LogInPassword(controller: _passwordController),
-            ),
-            const SizedBox(height: 20),
-            ValidationButton(onValidate: () => _validateAndNavigate(context)),
-          ],
-        ),
+          ),
+          const SizedBox(height: 16),
+          SizedBox(
+            width: 0.6 * MediaQuery.of(context).size.width,
+            child: LogInUsername(controller: _usernameController),
+          ),
+          const SizedBox(height: 16),
+          SizedBox(
+            width: 0.6 * MediaQuery.of(context).size.width,
+            child: LogInPassword(controller: _passwordController),
+          ),
+          const SizedBox(height: 20),
+          ValidationButton(onValidate: () => _validateAndNavigate(context)),
+        ],
       ),
     );
   }
